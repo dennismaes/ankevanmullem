3a7e593604a7c1aaf06c67dba90fb97db99887f6
Anke-dfx407 <53522114+Anke-dfx407@users.noreply.github.com>
2021-04-03 19:22:55 +0200

    Praktisch (kollom)

--- src/pages/praktisch.js
+++ src/pages/praktisch.js
@@ -14,10 +14,23 @@ const Praktisch = () => {
       </Sticky>
       <Section bgGrey id="aboutme">
         <h2>Waar?</h2>
-          <p>Mogelijkheid tot live coaching te Sint-Niklaas of coaching aan huis.
-          Bij coaching aan huis is het aangeraden om een rustige ruimte te voorzien waar wij ongestoord onze sessie kunnen houden, of een rustige wandelroute waarbij we buiten onze sessie kunnen houden.
-          <i>Voor coaching aan huis buiten Sint-Niklaas wordt het tarief gerekend plus 5 euro en een kilometervergoeding van € 0,35/km (herzien op april 2021).</i></p>
-          <p>Verder is er ook online coaching mogelijk. In tijden van Covid-19 heeft dit de voorkeur.</p>
+        <Wrapper cols={2}>
+        <div>
+            <h3>Live coaching</h3>
+            <p>Mogelijk tot live coaching te Sint-Niklaas <i>Kettermuitstraat 23, 9100 Sint-Niklaas</i></p>
+        </div>
+        <div>
+            <h3>Coaching aan huis</h3>
+            <p>Bij coaching aan huis is het aangeraden om een rustige ruimte te voorzien waar wij ongestoord onze sessie kunnen houden, 
+                of een rustige wandelroute in de buurt waarbij we buiten een sessie kunnen uitvoeren.</p>
+                <i>Voor coaching aan huis buiten Sint-Niklaas wordt het tarief gerekend plus 5 euro en een kilometervergoeding van € 0,35/km (herzien op april 2021).</i>
+        </div>
+        <div>
+            <h3>Online coaching</h3>
+            <p>Het is ook mogelijk een coaching online te volgen. In tijden van Covid-19 heeft dit de voorkeur om iedereen zijn gezondheid te waarborgen. 
+                Bij online coaching is het aangeraden om ook een rustige ruimte te voorzien waar jij ongestoord de sessie kan houden.</p>
+        </div>
+        </Wrapper>
       </Section>
       <Section id="aboutme">
           <h2>Wanneer?</h2>
