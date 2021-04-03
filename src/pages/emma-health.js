ba7d77b98fce9a26cfdea34586782b3adf5faf6c
Anke-dfx407 <53522114+Anke-dfx407@users.noreply.github.com>
2021-04-03 18:49:01 +0200

    Update emma-health.js

--- src/pages/emma-health.js
+++ src/pages/emma-health.js
@@ -18,7 +18,9 @@ const EmmaHealth = () => {
                 <p>Als je de beste versie van jezelf wilt worden, moet je eerst je DNA kennen voordat je je levensstijl te goed kan veranderen. 
                     Ben jij geïnteresseerd om meer inzicht te krijgen in de blauwdruk van je lichaam? </p>
                 <p>Via een innovatief partnerschap met Emma.health kan ik jou een DNA-analyse aanbieden met 2% korting. 
-                    Voor 391 euro (incl. BTW) krijg je meer informatie over 5 DNA-rapporten, een intake van jouw huidige levensstijl, een gesprek met een arts, alsook met een healthcoach.</p>
+                    Voor 391 euro (incl. BTW) krijg je meer informatie over 5 DNA-rapporten, een intake van jouw huidige levensstijl, een gesprek met een arts, alsook met een healthcoach.
+                    Sterker nog, boek je deze DNA-analyse in combinatie met enkele gerichte coachingsessies op maat bij mij, dan kan ik jou nog van een extra korting voorzien. 
+                    Bekijk zeker even de tarieven via deze <a href="https://ankevanmullem.be/praktisch/" target="_blank">link</a>. Indien je interesse hebt in een combinatie (DNA-analyse + coaching) stuur mij zeker even een bericht zodat ik jou kan verzekeren van deze korting.</p>
                 <p>Emma.health geeft geen diagnoses maar dient als een kompas om een optimale gezondheid te bekomen, specifiek op jouw lichaam geschreven. 
                     Het geeft jou meer specifiek inzicht om een leuker, langer en gezonder leven te leiden.
                     Meer weten? Neem een kijkje op hun website: <a href="https://emma.health/" target="_blank">Emma.health</a>, 
