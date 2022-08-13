c3528e526f9e50f1cdc2d5a36acfe0893947fbb8
Dennis Maes <dennis.maes@ugent.be>
2022-08-13 23:48:50 +0200

    Remove old page + custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,54 +1,23 @@
 import * as React from "react"
-import { Link } from "gatsby"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from "../components/layout";
+import { Sticky, Section } from "../components/intro";
+import { ButtonLink, Back } from "../components/button";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
-const NotFoundPage = () => {
+const NotFoundPage = ({ location }) => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+    <Layout locale="en" noTranslate location={location}>
+      <Section id="top">
+        <h2>Page not found</h2>
+      </Section>
+      <Sticky>
+        <ButtonLink to="/">
+          <Back />
+          Bring me back home
+        </ButtonLink>
+      </Sticky>
+    </Layout>
+  );
+};
 
 export default NotFoundPage
