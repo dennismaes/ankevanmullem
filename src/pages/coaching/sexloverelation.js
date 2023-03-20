6941611fa69475957f8190a77a1c3b12ed0b3cb1
Anke-dfx407 <53522114+Anke-dfx407@users.noreply.github.com>
2023-03-20 20:22:13 +0100

    import

--- src/pages/coaching/sexloverelation.js
+++ src/pages/coaching/sexloverelation.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import Layout from "../../components/layout";
 import { Section, Sticky, Wrapper } from "../../components/intro";
-import { Timeline, TimelineEntry } from "../components/timeline";
+import { Timeline, TimelineEntry } from "../../components/timeline";
 import {
   Button,
   ButtonLink,
